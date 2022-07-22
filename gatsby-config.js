64f42aa8fe116065f452567d961b6a0342b040b7
Nicanor008 <nicanor@Nicanors-MacBook-Pro.local>
2022-07-22 11:27:37 +0300

    Finer details touch on first deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Nicanor Korir',
     description:
-      'Nicanor Korir is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Nicanor Korir is a software engineer who specializes in building exceptional performant, accessible  and digital experiences.',
     siteUrl: 'http://nicanor.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@nicanor_nic',
